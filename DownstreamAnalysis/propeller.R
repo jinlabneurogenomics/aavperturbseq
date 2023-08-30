e1da9b502efa950d494d9171cc47aeee8c291dae
Kwanho Kim <kimkh415@gmail.com>
2023-08-30 18:24:09 +0000

    consider variability in the number of gRNA

--- DownstreamAnalysis/propeller.R
+++ DownstreamAnalysis/propeller.R
@@ -1,1 +1,110 @@
-../../proportion_change/propeller.R
+library(Seurat)
+library(speckle)
+library(limma)
+
+######
+# DONT USE THIS
+######
+# issues with duplicateCorrelation
+# assumptions that are probably best to avoid (the use of duplicateCorrelation is probably the main reason that pseudocell has issues--it doesn't fit a standard mixed model), namely that each variable (in this case each cell type) has the same amount of batch effect (in some sense), which is not likely to be a reasonable assumption
+# Also, it does not correctly estimate p-values (it does not take into account the uncertainty in the estimation of the random effects when calculating p-values, so leads to smaller p-values than it would get normally).
+# run propeller with random effect
+RunPropellerRand <- function(props, batch, treat, out.prefix="propeller_result") {
+# design matrix specification: (1) group information is always first and (2) there is NO intercept
+mm.treat = model.matrix(~treat)
+dupcor = duplicateCorrelation(props$TransformedProps, design=mm.treat, block=batch)
+fit1 = lmFit(props$TransformedProps, design=mm.treat, block=batch, correlation=dupcor$consensus)
+fit1 <- eBayes(fit1)
+print(summary(decideTests(fit1)))
+res=topTable(fit1, number=Inf)
+print(res)
+write.table(res, paste0(out.prefix, ".tsv"), sep='\t', quote=F, row.names=T, col.names=NA)
+return(res)
+}
+
+
+my.propeller.ttest <- function(prop.list=prop.list, design=design,
+                            contrasts=contrasts, robust=robust, trend=trend,
+                            sort=sort)
+{
+    prop.trans <- prop.list$TransformedProps
+    prop <- prop.list$Proportions
+
+    fit <- lmFit(prop.trans, design)
+    fit.cont <- contrasts.fit(fit, contrasts=contrasts)
+    fit.cont <- eBayes(fit.cont, robust=robust, trend=trend)
+
+    # Get mean cell type proportions and relative risk for output
+    # If no confounding variable included in design matrix
+    if(length(contrasts)==2){
+        fit.prop <- lmFit(prop, design)
+        z <- apply(fit.prop$coefficients, 1, function(x) x^contrasts)
+        RR <- apply(z, 2, prod)
+    }
+    # If confounding variables included in design matrix exclude them
+    else{
+        new.des <- design[,contrasts!=0]
+        fit.prop <- lmFit(prop,new.des)
+        new.cont <- contrasts[contrasts!=0]
+        z <- apply(fit.prop$coefficients, 1, function(x) x^new.cont)
+        RR <- apply(z, 2, prod)
+    }
+
+    fdr <- p.adjust(fit.cont$p.value, method="BH")
+
+    out <- data.frame(PropMean=fit.prop$coefficients, PropRatio=RR, limmaCoef=fit.cont$coefficients,
+                    Tstatistic=fit.cont$t[,1], P.Value=fit.cont$p.value[,1],
+                    FDR=fdr)
+    if(sort){
+        o <- order(out$P.Value)
+        out[o,]
+    }
+    else out
+}
+
+
+
+
+# Add batch as fixed effect
+RunPropeller <- function(props, meta, print.res=T, out.prefix="propeller_result", form="~ 0 + treat + batch") {
+design <- model.matrix(as.formula(form))
+#print(design)
+contrast.cmd = paste0("makeContrasts(", paste0('treat',levels(meta$treat)[2], '-treat',levels(meta$treat)[1]), ", levels=design)")
+print(contrast.cmd)
+my.contrast = eval(parse(text=contrast.cmd))
+#print(my.contrast)
+# robust = TRUE
+# robust empirical Bayes shrinkage of the variances is performed which mitigates the effects of outlying observations
+# trend = FALSE
+# as we don't expect a mean-variance trend after performing our variance stabilising transformation. 
+# There may also be an error when `trend` is set to TRUE because there are often not enough data points to estimate the trend.
+res = my.propeller.ttest(prop.list=props, design=design, contrasts=my.contrast, robust=TRUE, trend=FALSE, sort=TRUE)
+if (print.res) print(res)
+write.table(res, paste0(out.prefix, ".tsv"), sep='\t', quote=F, row.names=T, col.names=NA)
+return(res)
+}
+
+
+
+library(dplyr)
+library(ggplot2)
+# sample column
+# treat column
+# cell_type column
+MyBar <- function(df, sig.index) {
+xface = rep('plain',nlevels(df$clusters))
+xface[sig.index] = 'bold'
+xsize = rep(8, nlevels(df$clusters))
+xsize[sig.index] = 12
+p=df %>%
+  ggplot(aes(x=clusters, y=mean, fill=guide))+
+  geom_bar(stat = "identity", position = position_dodge())+
+  #geom_text(aes(label = paste(round(mean,2),"%")), position = position_dodge(0.9), vjust =-2, size = 3)+
+  geom_errorbar(aes(ymin=mean-ic, ymax=mean+ic), width=0.3, position=position_dodge(0.9))+
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  theme(axis.text.x = element_text(face=xface, size=xsize))
+  #scale_y_continuous(limits = c(-2,60))
+return(p)
+}
+
--- DownstreamAnalysis/run_sva_edger.R
+++ DownstreamAnalysis/run_sva_edger.R
@@ -1,1 +1,54 @@
-../../DEG/run_sva_edger.R
+# sva + edgeR script
+
+
+RunSVAedgeR <- function(dat.bulk, meta.dat, form="~assignment+orig.ident", form2="~orig.ident", name='test', ref='GFP') {
+require(edgeR)
+require(stringr)
+require(sva)
+require(EnhancedVolcano)
+# LRT models
+mod = model.matrix(as.formula(form), data=meta.dat)  # full model
+mod0 = model.matrix(as.formula(form2), data=meta.dat)  # null model
+# SVA
+#n.sv = num.sv(as.matrix(dat.bulk), mod)  # number of SVs
+n.sv=1
+modSv = mod
+#if (n.sv > 0) {
+#if (n.sv > 1) {print("Only include one surrogate variable");n.sv=1}
+svobj = sva(dat.bulk, mod, mod0, n.sv=n.sv)
+svs = svobj$sv
+colnames(svs) = paste0("SV", 1:ncol(svs))
+modSv = cbind(mod, svs)  # add SVs to model design
+#}
+# edgeR
+dge = DGEList(counts=dat.bulk, samples=meta.dat$sample, group=meta.dat$assignment)
+dge = estimateDisp(dge, modSv)
+fit = glmFit(dge, modSv)
+res = glmLRT(fit, coef=2)  # coef=2
+print(summary(decideTests(res)))
+pval = res$table$PValue
+# p-value histogram
+pdf(paste0("hist.", name, '.pdf'))
+print(hist(pval, main=name, ylab='nGene'))
+dev.off()
+# adjust p-value
+padj = p.adjust(pval, method='BH')
+res$table$padj = padj
+# volcano plot
+pdf(paste0("volcano.", name, ".pdf"))
+print(EnhancedVolcano(res$table,
+    lab = rownames(res$table),
+    x = 'logFC',
+    y = 'padj',
+    pCutoff=0.05,
+    FCcutoff=0.1,
+    subtitle=name))  #bquote(italic(.(name)))
+dev.off()
+# save result table
+write.table(res$table, paste0("edgeR_LRT_with_sva.", name, ".tsv"), sep='\t', quote=F, row.names=T, col.names=NA)
+pval.genes = rownames(res$table)[which(pval<0.05)]
+padj.genes = rownames(res$table)[which(padj<0.05)]
+return(list(pval.genes, padj.genes))
+}
+
+
