3c67d29a1ed3151abe2351e3505d21557afc05c1
joshpark00 <110137688+joshpark00@users.noreply.github.com>
2023-12-04 20:21:06 -0800

    Update annotations.R
    added code for plots from revision

--- AAV and PB related/annotations.R
+++ AAV and PB related/annotations.R
@@ -12,6 +12,7 @@ bed <- cbind(reads$chr1_name, reads$pos1, reads$pos1)
 write.table(bed, file = 'junctions.bed', sep = '\t', col.names = FALSE, row.names = FALSE)
 
 peak <- readPeakFile('junctions.bed')
+peak <- peak[!duplicated(peak)]
 peakAnno <- annotatePeak('junctions.bed', tssRegion=c(-3000, 3000),
                          TxDb=txdb, annoDb="org.Mm.eg.db")
 
@@ -31,3 +32,50 @@ write.csv(df, 'chipseeker.csv')
 anno <- df$annotation
 anno[str_detect(anno, 'Intron')] <- 'Intron'
 table(anno) %>% as.data.frame() %>% write.csv('junction_counts.csv')
+
+## Plot background distribution (
+background <- readPeakFile('insertSequences.bed') # BED file of all TTAATTAA seqences in genome
+backgroundAnno <- annotatePeak(background, tssRegion=c(-3000, 3000),
+                         TxDb=txdb, annoDb="org.Mm.eg.db")
+
+pdf('backgroundPie.pdf')
+plotAnnoPie(backgroundAnno)
+dev.off()
+
+pdf('backgroundVenn.pdf')
+vennpie(backgroudAnno)
+dev.off()
+
+anno_df <- as.data.frame(backgroundAnno)
+anno <- anno_df$annotation
+anno[str_detect(anno, 'Intron')] <- 'Intron'
+anno[str_detect(anno, 'Exon')] <- 'Exon'
+table(anno) %>% as.data.frame() %>% write.csv('background_counts.csv')
+
+## Comparison between insertions and background
+df <- read.csv('background_counts.csv')
+df2 <- read.csv('insert_counts.csv')
+df <- full_join(df, df2, by = 'anno', suffix = c("_background", "_insert"))
+df %>% write.csv('joined_counts.csv')
+
+back_sum <- sum(df$Freq_background)
+insert_sum <- sum(df$Freq_insert, na.rm = TRUE)
+df %<>% mutate(Percent_background = Freq_background / back_sum, 
+               Percent_insert = Freq_insert / insert_sum) %>%
+  dplyr::select(!starts_with("X_")) %>%
+  pivot_longer(!anno)
+
+df %>% 
+  filter(name %in% c('Percent_background', 'Percent_insert'), anno != 'Downstream (<=300bp)') %>%
+  ggplot() +
+  geom_bar(mapping = aes(x = anno, y = value, fill = name), stat = 'identity', position = 'dodge') +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  ylab('fraction of insertions')
+ggsave('insert_vs_background.pdf')
+
+df2 %>% ggplot() +
+  geom_bar(mapping = aes(x = anno, y = Freq), stat = 'identity') + 
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave('insert.pdf')
